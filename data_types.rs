214e4c6dc512d73a1417a1c0743a46945566c399
Davoud Keramati <davoud.keramati@gmail.com>
2026-08-17 15:43:18 +0330

    Create data_types.rs
    RUST TUTORIAL => data types

---
+++ data_types.rs
@@ -0,0 +1,29 @@
+/*
+************ RUST TUTORIAL ************
+TITLE: DATA TYPES
+*/
+
+fn main(){
+    let name = "davoud";
+    let family = "keramati";
+    let nickname = "daker";
+    let first_letter_of_name = 'D';
+    let age = 20;
+    let national_code = "00103050654";
+    let weight = 80.350;
+    let height = 180;
+    let married = false;
+    let isAlive = true;
+
+    println!("information => name:{},
+    family:{},
+    nickname:{},
+    first letter of name:{},
+    age:{},
+    national code:{},
+    weight:{},
+    height:{},
+    married:{},
+    isAlive:{}"
+    ,name,family,nickname,first_letter_of_name,age,national_code,weight,height,married,isAlive)
+}
